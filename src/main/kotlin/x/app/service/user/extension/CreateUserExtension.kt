507967a04bff05754f6d05b55f658a85a48cd401
Jiahua.Zhang <i@iamee.me>
2019-05-03 00:24:59 +0800

    在创建用户之前需要创建账户

--- src/main/kotlin/x/app/service/user/extension/CreateUserExtension.kt
+++ src/main/kotlin/x/app/service/user/extension/CreateUserExtension.kt
@@ -1,6 +1,9 @@
 package x.app.service.user.extension
 
+import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.modelling.command.Repository
+import x.app.common.account.command.CreateAccountCommand
+import x.app.common.dsl.sendTo
 import x.app.common.user.command.CreateUserCommand
 import x.app.service.user.User
 import x.app.utils.extension.annotation.Extension
@@ -13,10 +16,12 @@ import x.app.utils.extension.point.ICommandExtensionPoint
  *   @Date:    2019-05-01 23:20
  */
 @Extension(CreateUserCommand::class)
-class CreateUserExtension : ICommandExtensionPoint<CreateUserCommand, User> {
+class CreateUserExtension(
+        private val commandGateway: CommandGateway
+) : ICommandExtensionPoint<CreateUserCommand, User> {
 
     override fun before(repository: Repository<User>, command: CreateUserCommand) {
-
+        CreateAccountCommand(accountId = command.accountId, accountType = command.accountType, userId = command.userId) sendTo commandGateway
     }
 
 }
